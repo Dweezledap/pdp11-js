b93bbcaa11b6b38875f3d0094f51e9876d211bd6
takahiro <hogehoge@gachapin.jp>
2013-07-27 05:29:22 +0900

    added TextAreaview. and added some features.

---
+++ pdp11/TextAreaView.js
@@ -0,0 +1,18 @@
+function TextAreaView( id ) {
+  this.view = window.document.getElementById( id ) ;
+  this.clear( ) ;
+}
+
+TextAreaView.prototype.output = function( str ) {
+  this.view.firstChild.appendData( str ) ;
+  this.view.scrollTop = this.view.scrollHeight ;
+} ;
+
+TextAreaView.prototype.outputLine = function( str ) {
+  this.output( str + '\n' ) ;
+} ;
+
+TextAreaView.prototype.clear = function( ) {
+  this.view.firstChild.deleteData( 0, this.view.firstChild.nodeValue.length ) ;
+} ;
+
